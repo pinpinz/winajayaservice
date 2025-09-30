2490b21313f2294018d0795e3454442457c07c87
pinpinz <kvinnn12@gmail.com>
2025-09-30 10:07:29 +0700

    update  service Report

--- connect.php
+++ connect.php
@@ -13,3 +13,21 @@ try {
     die(json_encode(["status" => "error", "message" => $e->getMessage()]));
 }
 ?>
+
+<?php
+$server   = "localhost"; // biasanya cukup 'localhost' di shared hosting
+$database = "pureeid_Winajaya";
+$username = "pureeid_admin";
+$password = "Adminpuree12";
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Jika berhasil, bisa tampilkan pesan (opsional)
+    // echo json_encode(["status" => "success", "message" => "Connected to MySQL"]);
+} catch (PDOException $e) {
+    die(json_encode(["status" => "error", "message" => $e->getMessage()]));
+}
+?>
+
